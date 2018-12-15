1784804ae83e832eeb3a210a1e4e2ef8bb557a5d
Greavesy1899 <connor.greaves1999@gmail.com>
2018-12-15 05:21:17 +0000

    Removed useless resx file

--- Mafia2Libs/Properties/Resources.Designer.cs
+++ Mafia2Libs/Properties/Resources.Designer.cs
@@ -61,16 +61,6 @@ namespace Mafia2Tool.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap Splash {
-            get {
-                object obj = ResourceManager.GetObject("Splash", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
         /// </summary>
         internal static System.Drawing.Icon Toolkit {
