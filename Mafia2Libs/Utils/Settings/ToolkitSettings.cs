96a1ab256e4aa87ec8e0f17a8bcde6c4b35b4f6c
Greavesy1899 <connor.greaves1999@gmail.com>
2020-05-30 15:54:40 +0100

    Updated version variable

--- Mafia2Libs/Utils/Settings/ToolkitSettings.cs
+++ Mafia2Libs/Utils/Settings/ToolkitSettings.cs
@@ -46,7 +46,7 @@ namespace Utils.Settings
         public static bool CheckForUpdates;
         public static bool SkipGameSelector;
         public static int DefaultGame;
-        public static readonly float Version = 2.04f;
+        public static readonly float Version = 2.05f;
 
         public static void ReadINI()
         {
