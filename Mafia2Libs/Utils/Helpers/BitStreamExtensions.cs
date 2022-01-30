19477edaceb7d6aca3330cc97bfb42eb77f513fe
Quarkblitz <quarkblitz@dev.null>
2022-01-30 19:43:49 +0100

    Fix ReadSingleUnorm()

--- Mafia2Libs/Utils/Helpers/BitStreamExtensions.cs
+++ Mafia2Libs/Utils/Helpers/BitStreamExtensions.cs
@@ -15,7 +15,13 @@ namespace Mafia2Tool.Utils.Helpers
             System.Diagnostics.Debug.Assert(size > 0 && size <= 32);
             var maxValue = (float)((1u << size) - 1);
             var bytes = stream.ReadBytes(size);
-            var singlei = BitConverter.ToInt32(bytes);
+            var singlei = ((size + 7) / 8) switch
+            {
+                1 => (int)BitConverter.ToChar(bytes),
+                2 => (int)BitConverter.ToInt16(bytes),
+                3 => (int)BitConverter.ToInt16(bytes) | (int)BitConverter.ToChar(bytes, 2) << 16,
+                _ => BitConverter.ToInt32(bytes),
+            };
             return singlei / maxValue;
         }
     }
